f84984e5d7a42d4907ab80f846f00b4ba81dc141
Jacegk1999 <Jacegk1999@gmail.com>
2022-04-04 16:49:52 -0700

    working players reformat

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,28 +1,32 @@
 import React from 'react';
+import { useGameContext } from '../context/GameContext';
 import CardList from './CardList';
 
-export default function Player({
-  player,
-  setTo,
-  hand,
-  setFrom,
-  setSelectedCard,
-  selectedCard,
-  to,
-}) {
-  return (
-    <div
-      className={`player ${to === player ? 'selected-player' : ''}`}
-      onClick={() => setTo(player)}
-    >
-      <p>Player {player}</p>
-      <CardList
-        player={player}
-        cards={hand}
-        setFrom={setFrom}
-        selectedCard={selectedCard}
-        setSelectedCard={setSelectedCard}
-      />
-    </div>
-  );
+export default function Player({ player, hand }) {
+  const { to, setFrom, selectedCard, setTo, setSelectedCard } = useGameContext();
+
+  // player,
+  // setTo,
+  // hand,
+  // setFrom,
+  // setSelectedCard,
+  // selectedCard,
+  // to,
+  {
+    return (
+      <div
+        className={`player ${to === player ? 'selected-player' : ''}`}
+        onClick={() => setTo(player)}
+      >
+        <p>Player {player}</p>
+        <CardList
+          player={player}
+          cards={hand}
+          setFrom={setFrom}
+          selectedCard={selectedCard}
+          setSelectedCard={setSelectedCard}
+        />
+      </div>
+    );
+  }
 }
